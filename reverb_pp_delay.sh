edd6114750c833f19007f683b8aaead5f4c79a0d
Lorenzo Monaco <m3m3nto@gmail.com>
2026-06-11 00:35:35 +0200

    Added reverb + pp delay

---
+++ reverb_pp_delay.sh
@@ -0,0 +1,4 @@
+pd -nogui -alsa -rt -audiobuf 20 \
+-send "roomsize 0.95" -send "damping 0.3" -send "wet 0.5" \
+-send "delay_time 500" -send "delay_fb 0.8" -send "delay_mix 0.7" \
+/root/rpi-dsp/reverb.pd
